576e9da211816d501518d5d6ea41ace737005430
Natalie Clamp <35259261+NatClamp@users.noreply.github.com>
2020-01-03 20:17:32 +0000

    update footer reference

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,8 @@ const Footer = () => {
     <footer>
       <div className="footer-container">
         <p className="pantheria-ref">
-          Nathan P. Myhrvold, Elita Baldridge, Benjamin Chan, Dhileep Sivam,
-          Daniel L. Freeman, and S. K. Morgan Ernest. 2015. An amniote
-          life-history database to perform comparative analyses with birds,
-          mammals, and reptiles. Ecology 96:3109.{" "}
-          <a href="http://dx.doi.org/10.1890/15-0846.1">DOI</a>
+Kate E. Jones, Jon Bielby, Marcel Cardillo, Susanne A. Fritz, Justin O'Dell, C. David L. Orme, Kamran Safi, Wes Sechrest, Elizabeth H. Boakes, Chris Carbone, Christina Connolly, Michael J. Cutts, Janine K. Foster, Richard Grenyer, Michael Habib, Christopher A. Plaster, Samantha A. Price, Elizabeth A. Rigby, Janna Rist, Amber Teacher, Olaf R. P. Bininda-Emonds, John L. Gittleman, Georgina M. Mace, and Andy Purvis. 2009. PanTHERIA: a species-level database of life history, ecology, and geography of extant and recently extinct mammals. Ecology 90:2648.
+          <a href="http://esapubs.org/archive/ecol/E090/184/">DOI</a>
         </p>
         <p>
           This application was created by{" "}
