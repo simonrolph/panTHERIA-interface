bee103b31f10b4e4306d764121dd6f4f077e317f
NatClamp <nat.clamp@gmail.com>
2020-03-23 12:26:47 +0000

    feature: begin data card layout

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -5,7 +5,7 @@ const DataCard = (props) => {
 
   const dataItems = Object.keys(data).map(key => {
     return (
-      <div className="dataItem" key={key}>
+      <div className={"dataItem " + ((typeof(data[key]) === 'string') ? 'dataItem--text' : 'dataItem--number' )} key={key}>
         <h3>{key}</h3>
         {data[key] === 'Unknown' || data[key] === undefined ? null : <p>{data[key]}</p>}
       </div>
@@ -14,8 +14,12 @@ const DataCard = (props) => {
 
   return ( 
     <article className="dataCard">
-      <h2>{title}</h2>
-      {dataItems}
+      <h2 className="dataCard__title">
+        <div className="dataCard__title__container">
+          {title}
+        </div>
+      </h2>
+      <div className="dataCard__dataItems">{dataItems}</div>
     </article>
    );
 }
