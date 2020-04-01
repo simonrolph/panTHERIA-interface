484d444e514f704230baf975c22b6e5d26e0e488
NatClamp <nat.clamp@gmail.com>
2020-04-01 16:09:18 +0100

    fix: add mammal id and species rank to inaturalist api query to ensure results are accurate

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,9 +8,9 @@ exports.getIUCNstatus = (endpoint) => {
 }
 
 exports.getiNaturalistData = (speciesBinomial) => {
-  return axios.get(`${iNaturalistBaseURL}taxa?q=${speciesBinomial}`).then((result) => {
+  return axios.get(`${iNaturalistBaseURL}taxa?q=${speciesBinomial}&taxon_id=40151&rank=species`).then((result) => {
     let records = result.data.results;
-    if (records.length) {
+    if (records.length ) {
       let image = records[0].default_photo ? records[0].default_photo.medium_url : null;
       let commonName = records[0].preferred_common_name;
     return { image, commonName };
